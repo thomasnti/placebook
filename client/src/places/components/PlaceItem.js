82b005d30f748a0a274349f8c701638a04628641
Thomas Ntinas <tdinas@softone.gr>
2022-01-14 17:15:48 +0200

    added google maps modal

--- client/src/places/components/PlaceItem.js
+++ client/src/places/components/PlaceItem.js
@@ -1,28 +1,50 @@
-import React from "react";
+import React, { useState } from 'react';
 
-import Card from "../../shared/components/UIElements/Card";
-import Button from '../../shared/components/FormElements/Button'
-import "./PlaceItem.css";
+import Card from '../../shared/components/UIElements/Card';
+import Button from '../../shared/components/FormElements/Button';
+import Modal from '../../shared/components/UIElements/Modal';
+import './PlaceItem.css';
 
 function PlaceItem(props) {
+  const [showMap, setShowMap] = useState(false);
+
+  const openMapHandler = () => setShowMap(true);
+  const closeMapHandler = () => setShowMap(false);
+
   return (
-    <Card className="place-item__content">
-      <li className="place-item">
-        <div className="place-item__image">
-          <img src={props.image} alt={props.title} />
+    <React.Fragment>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>Close</Button>}
+      >
+        <div className="map-container">
+          <h2>Here i will render the google map</h2>
         </div>
-        <div className="place-item__info">
-          <h2>{props.title}</h2>
-          <h3>{props.address}</h3>
-          <p>{props.description}</p>
-        </div>
-        <div className="place-item__actions">
-          <Button inverse>View on Map</Button>
-          <Button to={`/places/${props.id}`}>Edit</Button>
-          <Button danger>Delete</Button>
-        </div>
-      </li>
-    </Card>
+      </Modal>
+      <Card className="place-item__content">
+        <li className="place-item">
+          <div className="place-item__image">
+            <img src={props.image} alt={props.title} />
+          </div>
+          <div className="place-item__info">
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+          </div>
+          <div className="place-item__actions">
+            <Button inverse onClick={openMapHandler}>
+              View on Map
+            </Button>
+            <Button to={`/places/${props.id}`}>Edit</Button>
+            <Button danger>Delete</Button>
+          </div>
+        </li>
+      </Card>
+    </React.Fragment>
   );
 }
 
