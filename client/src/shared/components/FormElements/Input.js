359162f8a24e47dfd6134e6cc3e3f1008e91f983
Thomas Ntinas <tdinas@softone.gr>
2022-01-17 16:49:14 +0200

    custom input component

---
+++ client/src/shared/components/FormElements/Input.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './Input.css';
+
+function Input(props) {
+  const element =
+    props.element === 'input' ? (
+      <input type={props.type} id={props.id} placeholder={props.placeholder} />
+    ) : (
+      <textarea id={props.id} rows={props.rows || 4}></textarea>
+    );
+
+  // for is htmlFor in React
+  return (
+    <div className='form-control'>
+      <label htmlFor={props.id}>{props.label}</label>
+      {element}
+    </div>
+  );
+}
+
+export default Input;
