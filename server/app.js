b4ed8d18f402d0f43aeb21d7f7e88f04ce555f01
Thomas Ntinas <tdinas@softone.gr>
2022-01-19 16:51:31 +0200

    test http requests from vscode

--- server/app.js
+++ server/app.js
@@ -7,4 +7,6 @@ const app = express();
 
 app.use(placesRoutes); // we pass placesRoutes as middleware to our app
 
-app.listen(5000);
+app.listen(5000, () => {
+  console.log("Placebook's server is listening on port 5000");
+});
