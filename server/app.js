76bec2127d01afad6e5967f042e87928f0138321
Thomas Ntinas <tdinas@softone.gr>
2022-01-21 16:36:15 +0200

    debug inside vscode

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const placesRoutes = require('./routes/places-routes');
 
 const app = express();
 
-app.use(placesRoutes); // we pass placesRoutes as middleware to our app
+app.use('/api/places', placesRoutes); // api/places = path (route) for which the middleware function applies.
 
 app.listen(5000, () => {
   console.log("Placebook's server is listening on port 5000");
